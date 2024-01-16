ac8c377d3cc358079a1c164d3348081a11d66821
amriarendy <amriarendy90@gmail.com>
2024-01-16 23:52:37 +0700

    reactjs static

---
+++ README.md
@@ -0,0 +1,30 @@
+### Reactjs Static
+
+### Install depedencies react npm
+
+- `npm install`
+
+### Run App
+
+- `cd directory_apps`
+- `npm start`
+
+### running database with json-server
+
+- running database in terminal `npx json-server --watch db.json --port 8080`
+
+### depedencies
+
+```json
+  "dependencies": {
+    "@testing-library/jest-dom": "^5.16.5",
+    "@testing-library/react": "^13.4.0",
+    "@testing-library/user-event": "^13.5.0",
+    "bulma": "^0.9.4",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-router-dom": "^5.2.0",
+    "react-scripts": "5.0.1",
+    "web-vitals": "^2.1.4"
+  },
+```
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ProductList from './components/ProductList';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h1>Halaman Utama</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <ProductList/>
+          </Route>
+          <Route path="/add">
+            <AddProduct/>
+          </Route>
+          <Route path="/edit/:id">
+            <EditProduct/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
