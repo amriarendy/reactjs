cc919cd0c9d298f1eba3fc36ebfc51944e2e727b
amriarendy <amriarendy90@gmail.com>
2024-01-18 14:55:25 +0700

    react login

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    // Fetch the user email and token from local storage
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    // If the token/email does not exist, mark the user as logged out
+    if (!user || !user.token) {
+      setLoggedIn(false);
+      return;
+    }
+
+    // If the token exists, verify it with the auth server to see if it is valid
+    fetch("http://localhost:8000/verify", {
+      method: "POST",
+      headers: {
+        "jwt-token": user.token,
+      },
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        setLoggedIn("success" === r.message);
+        setEmail(user.email || "");
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
