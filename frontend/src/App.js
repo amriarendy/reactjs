be25c24af67db4afd180abcdac41d1c75f95dcac
amriarendy <amriarendy90@gmail.com>
2024-01-28 21:36:51 +0700

    reactjs cashier

---
+++ frontend/src/App.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Result, NavbarComponents, ListCategories, Menus } from "./components";
+import { API_URL } from "./utils/constants";
+import axios from "axios";
+import swal from "sweetalert";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+      categorySelected: "Makanan",
+      carts: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products?category.nama=" + this.state.categorySelected)
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  changeCategory = (value) => {
+    this.setState({
+      categorySelected: value,
+      menus: [],
+    });
+    axios
+      .get(API_URL + "products?category.nama=" + value)
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  checkOut = (value) => {
+    axios
+      .get(API_URL + "keranjangs?product.id=" + value.id)
+      .then((res) => {
+        if (res.data.length === 0) {
+          const cart = {
+            jumlah: 1,
+            total_harga: value.harga,
+            product: value,
+          };
+
+          axios
+            .post(API_URL + "keranjangs", cart)
+            .then((res) => {
+              swal({
+                title: "Success",
+                text: "Success Create Data! " + cart.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1000,
+              });
+            })
+            .catch((error) => {
+              console.log("Error: ", error);
+            });
+        } else {
+          const cart = {
+            jumlah: res.data[0].jumlah + 1,
+            total_harga: res.data[0].total_harga + value.harga,
+            product: value,
+          };
+          axios
+            .put(API_URL + "keranjangs/" + res.data[0].id, cart)
+            .then((res) => {
+              swal({
+                title: "Success",
+                text: "Success Create Data! " + cart.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1000,
+              });
+            })
+            .catch((error) => {
+              console.log("Error: ", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { menus, categorySelected } = this.state;
+    return (
+      <div className="App">
+        <NavbarComponents />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories
+                changeCategory={this.changeCategory}
+                categorySelected={categorySelected}
+              />
+              <Col>
+                <h4>
+                  <strong>List Product</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus &&
+                    menus.map((menu) => (
+                      <Menus
+                        key={menu.id}
+                        menu={menu}
+                        checkOut={this.checkOut}
+                      />
+                    ))}
+                </Row>
+                <hr />
+              </Col>
+              <Result />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/components/index.js
@@ -0,0 +1,6 @@
+import Result from "./Result";
+import ListCategories from "./ListCategories";
+import NavbarComponents from "./NavbarComponents";
+import Menus from "./Menus";
+
+export { Result, ListCategories, NavbarComponents, Menus };
