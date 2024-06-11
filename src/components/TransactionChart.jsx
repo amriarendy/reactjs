474a0f3885fa0b7240a31e801bf47cb5dd0be686
amriarendy <amriarendy90@gmail.com>
2024-06-11 15:40:19 +0700

    create popular, and recent oders

--- src/components/TransactionChart.jsx
+++ src/components/TransactionChart.jsx
@@ -66,18 +66,21 @@ const data = [
 
 export default function TransactionChart() {
     return (
-        <div className="">
-            <ResponsiveContainer widht="100%" height="100%">
-                <BarChart width={500} height={300} data={data} margin={{top: 20, right: 20, left: -10, bottom: 0}}>
-                <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Bar dataKey="Income" fill="#0ea5e9" />
-						<Bar dataKey="Expense" fill="#ea580c" />
-                </BarChart>
-            </ResponsiveContainer>
+        <div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
+            <strong className="text-gray-700 font-medium">Transactions</strong>
+            <div className="w-full mt-3 flex-1 text-xs">
+                <ResponsiveContainer widht="100%" height="100%">
+                    <BarChart width={500} height={300} data={data} margin={{top: 20, right: 20, left: -10, bottom: 0}}>
+                    <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="Income" fill="#0ea5e9" />
+                            <Bar dataKey="Expense" fill="#ea580c" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     )
 }
